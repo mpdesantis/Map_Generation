29fdc28fceebbd988d7036fd1ac5ae1d882e15c7
Michael De Santis <michaelpeterdesantis@gmail.com>
2025-03-23 12:09:33 -0400

    implement argument parsing

--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,8 @@
 #include <chrono>
 #include <fstream>
 #include <string>
+#include <iomanip>
+#include <iostream>
 
 // Other
 #include "nlohmann/json.hpp"
@@ -17,6 +19,21 @@
 using namespace cadmium::celldevs;
 using namespace cadmium;
 
+// Print help menu
+void printHelp() {
+    std::cout << "\n\t*** Cell-DEVS Map Generation Utility ***\n";
+    std::cout << "\nUsage: map_generation [OPTIONS]";
+    std::cout << "\n\nOptions:";
+    std::cout << "\n  --config, -c     FILE   Take configuration from FILE.";
+    std::cout << "\n                            Default: config/mapgen_config.json";
+    std::cout << "\n  --duration, -d   NUM    Execute simulation for NUM time units.";
+    std::cout << "\n                            Default: 10";
+    std::cout << "\n  --output, -o     FILE   Log CSV output to FILE.";
+    std::cout << "\n                            Default: output/mapgen_grid_log.csv";
+    std::cout << "\n  --help, -h              Print this menu and exit.";
+    std::cout << std::endl;
+}
+
 // Model
 std::shared_ptr<GridCell<MapgenState, double>> addGridCell(const coordinates & cellId, const std::shared_ptr<const GridCellConfig<MapgenState, double>>& cellConfig) {
 	auto cellModel = cellConfig->cellModel;
@@ -31,24 +48,69 @@ std::shared_ptr<GridCell<MapgenState, double>> addGridCell(const coordinates & c
 // Simulation
 int main(int argc, char ** argv) {
 
-    /* Constants and Defaults */
+    /* Constants */
     const std::string DEFAULT_CONFIG_FILE_PATH = "config/mapgen_config.json";
     constexpr int DEFAULT_SIM_TIME = 10;
     const std::string DEFAULT_OUTPUT_FILE_PATH = "output/mapgen_grid_log.csv";
 
+    /* Defaults */
+	std::string configFilePath = DEFAULT_CONFIG_FILE_PATH;
+	double simTime = DEFAULT_SIM_TIME;
+	std::string outputFilePath = DEFAULT_OUTPUT_FILE_PATH;
+
     /* Argument Parsing */
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        // 1. Configuration File (JSON)
+        if (arg == "--config" || arg == "-c") {
+            if (i + 1 < argc) {
+                configFilePath = argv[++i];
+            }
+            else {
+                std::cout << "Missing argument for switch: [--config | -c]" <<std::endl;
+                return 1;
+            }
+        }
+        // 2. Simulation Duration (double)
+        else if (arg == "--duration" || arg == "-d") {
+            if (i + 1 < argc) {
+                simTime = std::stod(argv[++i]);
+            }
+            else {
+                std::cout << "Missing argument for switch: [--duration | -d]" <<std::endl;
+                return 1;
+            }
+        }
+        // 3. Output File (JSON)
+        else if (arg == "--output" || arg == "-o") {
+            if (i + 1 < argc) {
+                outputFilePath = argv[++i];
+            }
+            else {
+                std::cout << "Missing argument for switch: [--output | -o]" <<std::endl;
+                return 1;
+            }
+        }
+        // 4. Help Menu
+        else if (arg == "--help" || arg == "-h") {
+            printHelp();
+            return 0;
+        }
+        // Invalid Argument
+        else {
+            std::cout << "Invalid argument: " << arg <<std::endl;
+            printHelp();
+            return 1;
+        }
 
-    // 1. Configuration File (JSON)
-	std::string configFilePath = (argc > 1) ? argv[1] : DEFAULT_CONFIG_FILE_PATH;
-    std::cout << "Config File: " << configFilePath << std::endl;
+    }
 
-    // 2. Simulation Duration (double)
-	double simTime = (argc > 2) ? std::stod(argv[2]) : DEFAULT_SIM_TIME;
-    std::cout << "Simulation Duration: " << simTime << std::endl;
-
-    // 3. Output File (JSON)
-	std::string outputFilePath = (argc > 3) ? argv[3] : DEFAULT_OUTPUT_FILE_PATH;
-    std::cout << "Output File: " << outputFilePath << std::endl;
+    // Print simulation parameters to console
+    std::cout << "\nAttempting simulation with the following parameters:";
+    std::cout << "\n\tConfig File: " << configFilePath;
+    std::cout << "\n\tSimulation Duration: " << simTime;
+    std::cout << "\n\tOutput File: " << outputFilePath;
+    std::cout << std::endl;
 
     /* Models */
 	auto model = std::make_shared<GridCellDEVSCoupled<MapgenState, double>>("mapgen", addGridCell, configFilePath);
@@ -59,8 +121,13 @@ int main(int argc, char ** argv) {
 	rootCoordinator.setLogger<CSVLogger>(outputFilePath, ";");
 	
     /* Simulation */
+
+    // Run simulation
 	rootCoordinator.start();
 	rootCoordinator.simulate(simTime);
 	rootCoordinator.stop();
+
+    // Report results and return
+    std::cout << "\nSimulation complete.\n";
     return 0;
 }
